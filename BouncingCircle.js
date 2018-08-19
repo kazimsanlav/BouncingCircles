472bc1d8d187e4fad2030c5907e93caed865f29f
kazimsanlav <kazimsanlav@gmail.com>
2018-08-19 15:54:11 +0300

    Add rainbow option, remove stroke

--- BouncingCircle.js
+++ BouncingCircle.js
@@ -1,6 +1,7 @@
 let Circles = [];
 let MAXCIRCLES = 25;
-let cnv, p1, p2, p3, b1, s1, s2;
+let cnv, p1, p2, p3, b1, b2, s1, s2;
+let colorchange = false;
 
 function setup() {
     //canvas
@@ -11,8 +12,13 @@ function setup() {
     p1 = createP('Number of balls: ' + Circles.length);
     // button1
     b1 = createButton('Reset');
-    b1.position(150, windowHeight - 40);
+    createP('');
+    // b1.position(150, windowHeight - 40);
     b1.mouseClicked(resetCanvas);
+    // button2
+    b2 = createButton('Rainbow on/off');
+    // b2.position(150, windowHeight - 40);
+    b2.mouseClicked(rainbow);
     //paragraph2 and sileder1
     p2 = createP('Radius:');
     // p2.position(310, windowHeight - 55);
@@ -30,9 +36,14 @@ function resetCanvas() {
     background(0);
 }
 
+function rainbow() {
+    colorchange = !colorchange;
+    // print(colorchange);
+}
+
 function draw() {
 
-    background(0);
+    background(0,25);
 
     p1.html('Number of balls: ' + Circles.length);
     // update show every circle
@@ -79,19 +90,39 @@ function draw() {
 }
 
 function addCircle() {
-    Circles.push(new Circle(mouseX, mouseY, s1.value(),
-        random([1, -1]) * s2.value(),
-        random([1, -1]) * s2.value()));
+    if (colorchange) {
+        Circles.push(new Circle(mouseX, mouseY, s1.value(), random(256), random(256), random(256),
+            random([1, -1]) * s2.value(),
+            random([1, -1]) * s2.value()));
+    } else {
+        Circles.push(new Circle(mouseX, mouseY, s1.value(), 200, 200, 200,
+            random([1, -1]) * s2.value(),
+            random([1, -1]) * s2.value()));
+    }
 }
 
 class Circle {
-    constructor(x, y, radius, vx, vy, col) {
+    // constructor(x, y, radius, vx, vy) {
+    //     this.x = x;
+    //     this.y = y;
+    //     this.radius = radius;
+    //     this.r = 200;
+    //     this.g = 200;
+    //     this.b = 200;
+    //     this.vx = vx;
+    //     this.vy = vy;
+    //     this.col = col;
+    // }
+
+    constructor(x, y, radius, r = 200, g = 200, b = 200, vx, vy) {
         this.x = x;
         this.y = y;
         this.radius = radius;
+        this.r = r;
+        this.g = g;
+        this.b = b;
         this.vx = vx;
         this.vy = vy;
-        this.col = col;
     }
 
     update() {
@@ -109,8 +140,9 @@ class Circle {
 
     show() {
         strokeWeight(2);
-        stroke(255);
-        fill(this.col);
+        noStroke();
+        // stroke(255);
+        fill(this.r, this.g, this.b);
         ellipse(this.x, this.y, this.radius, this.radius);
     }
 
